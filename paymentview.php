8ca85df8ad6ec333ec734685e3d6fc06b0a22a08
kar66-star <94402493+kar66-star@users.noreply.github.com>
2022-01-08 21:33:27 +0200

    Delete paymentview.php

--- paymentview.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	function showpaymentdetails($objpayment)
-	{
-		echo "student_id:" .objpayment->student_id."<br>";
-		echo "iscreated:" .objpayment->iscreated."<br>";
-		echo "paytype." .objpayment->paytype."<br>";
-	}
